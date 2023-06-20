a9f64abbae8fbcb67b0f38ce5174b059223c64eb
MOUADDIKOUK <mouad.wantedpubgm@gmail.com>
2023-06-20 10:57:28 -0700

    My commit

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,17 +9,21 @@
  */
 int print_sign(int n)
 {
-	if (n > 0)
-	{
-		_putchar('+');
-		return (1);
-	} else if (n == 0)
-}
-		_putchar(48);
-		return (0);
-	} else if (n < 0)
-	{
-		_putchar('-');
-	}
-		return (-1);
+        if (n > 0)
+        {
+                _putchar('+');
+                return (1);
+        }
+        else if (n == 0)
+        {
+                _putchar('0');
+                return (0);
+        }
+        else if (n < 0)
+        {
+                _putchar('-');
+                return (-1);
+        }
+        /* Add a default return statement in case none of the conditions are met */
+        return (0);
 }
